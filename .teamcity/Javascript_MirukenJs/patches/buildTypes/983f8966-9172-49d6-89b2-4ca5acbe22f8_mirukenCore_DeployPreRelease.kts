3d0b4373cd05117541f52cc14d1a370068d53162
provenstyle <provenstyle@TeamCity>
2018-02-01 21:45:53 +0000

    TeamCity change in 'Javascript :: Miruken-JS :: Core Project :: Deployment :: miruken-core' project: runners of 'Deploy PreRelease' build configuration were updated

--- .teamcity/Javascript_MirukenJs/patches/buildTypes/983f8966-9172-49d6-89b2-4ca5acbe22f8_mirukenCore_DeployPreRelease.kts
+++ .teamcity/Javascript_MirukenJs/patches/buildTypes/983f8966-9172-49d6-89b2-4ca5acbe22f8_mirukenCore_DeployPreRelease.kts
@@ -22,7 +22,7 @@ changeBuildType("983f8966-9172-49d6-89b2-4ca5acbe22f8_mirukenCore_DeployPreRelea
         insert(1) {
             script {
                 name = "Publish"
-                scriptContent = "%npm% publish --tag prerelease"
+                scriptContent = "%npm% publish %PackageName%-%PackageVersion%.tgz --tag prerelease"
             }
         }
     }
