d08f2d09f2f2f66580f790bbf5758ae265579300
michael dudley <michael.dudley@improvingenterprises.com>
2018-02-03 00:02:09 -0600

    description

--- .teamcity/Javascript_Packages/Project.kt
+++ .teamcity/Javascript_Packages/Project.kt
@@ -6,10 +6,11 @@ import jetbrains.buildServer.configs.kotlin.v2017_2.projectFeatures.VersionedSet
 import jetbrains.buildServer.configs.kotlin.v2017_2.projectFeatures.versionedSettings
 
 object Project : Project({
-    uuid     = "c3a61f40-54ed-43bc-9b0b-21a4a70c331c"
-    id       = "Javascript_Packages"
-    parentId = "Javascript"
-    name     = "Packages"
+    uuid        = "c3a61f40-54ed-43bc-9b0b-21a4a70c331c"
+    id          = "Javascript_Packages"
+    parentId    = "Javascript"
+    name        = "Packages"
+    description = "Configured with Kotlin"
 
     vcsRoot(Javascript_MirukenJs_TeamCityPackageBuilds)
 
