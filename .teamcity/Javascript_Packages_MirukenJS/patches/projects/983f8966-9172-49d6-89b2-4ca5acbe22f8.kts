839ac8854a265bb4fd51ca7c6caff927d82c2928
cneuwirt <cneuwirt@TeamCity>
2021-01-04 00:24:43 +0000

    TeamCity change in 'Javascript / Packages / Miruken-JS / Core Project' project: project parameters were changed

--- .teamcity/Javascript_Packages_MirukenJS/patches/projects/983f8966-9172-49d6-89b2-4ca5acbe22f8.kts
+++ .teamcity/Javascript_Packages_MirukenJS/patches/projects/983f8966-9172-49d6-89b2-4ca5acbe22f8.kts
@@ -21,7 +21,7 @@ changeProject("983f8966-9172-49d6-89b2-4ca5acbe22f8") {
             param("PatchVersion", "5")
         }
         update {
-            param("PatchVersion", "6")
+            param("PatchVersion", "0")
         }
     }
 }
