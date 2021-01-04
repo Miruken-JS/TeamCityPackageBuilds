826877c42f999b39ad1c24f130ecffde9acacfbf
cneuwirt <cneuwirt@TeamCity>
2021-01-04 00:24:24 +0000

    TeamCity change in 'Javascript / Packages / Miruken-JS / Core Project' project: project parameters were changed

--- .teamcity/Javascript_Packages_MirukenJS/patches/projects/983f8966-9172-49d6-89b2-4ca5acbe22f8.kts
+++ .teamcity/Javascript_Packages_MirukenJS/patches/projects/983f8966-9172-49d6-89b2-4ca5acbe22f8.kts
@@ -12,6 +12,12 @@ accordingly, and delete the patch script.
 changeProject("983f8966-9172-49d6-89b2-4ca5acbe22f8") {
     params {
         expect {
+            param("MajorVersion", "0")
+        }
+        update {
+            param("MajorVersion", "2")
+        }
+        expect {
             param("PatchVersion", "5")
         }
         update {
